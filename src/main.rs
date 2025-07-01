26bfe151ef23a13874d73f44baf8e34af4acc1b1
Ivar Stangeby <istangeby@gmail.com>
2025-07-01 15:38:33 +0200

    Add command line argument for file

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
 use crate::editor::Editor;
 use std::io;
+use std::path::{Path, PathBuf};
 
 mod editor;
 mod text_buffer;
 
 fn main() -> io::Result<()> {
-    let mut editor = Editor::new();
+    let args: Vec<String> = std::env::args().collect();
+
+    let mut editor = if args.len() > 1 {
+        let path = PathBuf::from(&args[1]);
+        match Editor::open_file(path) {
+            Ok(editor) => editor,
+            Err(e) => {
+                eprintln!("Error opening file: {} {}", &args[1], e);
+                std::process::exit(1);
+            }
+        }
+    } else {
+        Editor::new()
+    };
+
     match editor.run() {
         Ok(()) => {
             println!("editor exited successfully");
