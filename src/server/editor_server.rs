373c01f89e546ddbdeb1f5cc8697fbdfd9f85f8b
Ivar Stangeby <istangeby@gmail.com>
2025-07-02 13:07:09 +0200

    Tests compile now.

--- src/server/editor_server.rs
+++ src/server/editor_server.rs
@@ -1,61 +1,61 @@
-use crate::server::events::{ClientId, EditMode, ServerResult};
+use crate::server::events::{BufferId, ClientId, EditMode, EditorEvent, ServerResult};
 
 pub struct EditorServer {
 }
 
 impl EditorServer {
-    pub async fn set_edit_mode(&self, p0: _, p1: _) {
+    pub async fn set_edit_mode(&self, buffer_id: BufferId, mode: EditMode) -> ServerResult<()> {
         todo!()
     }
 
-    pub async fn get_edit_mode(&self, p0: _) -> EditMode {
+    pub async fn get_edit_mode(&self, buffer_id: BufferId) -> ServerResult<EditMode> {
         todo!()
     }
 
-    pub async fn get_client_events(&self, p0: _) {
+    pub async fn get_client_events(&self, client_id: ClientId) -> ServerResult<Vec<EditorEvent>> {
         todo!()
     }
 
-    pub async fn subscribe_to_buffer(&self, p0: _, p1: _) {
+    pub async fn subscribe_to_buffer(&self, client_id: ClientId, buffer_id: BufferId) -> ServerResult<()>{
         todo!()
     }
 
-    pub async fn move_cursor_left(&self, p0: _) {
+    pub async fn move_cursor_left(&self, buffer_id: BufferId) -> ServerResult<()> {
         todo!()
     }
-    pub async fn move_cursor_right(&self, p0: _) {
+    pub async fn move_cursor_right(&self, buffer_id: BufferId) -> ServerResult<()> {
         todo!()
     }
-    pub async fn get_cursor_position(&self, p0: _) -> usize {
-        todo!()
-    }
-
-    pub async fn set_cursor_position(&self, p0: _, p1: i32) {
+    pub async fn get_cursor_position(&self, buffer_id: BufferId) -> ServerResult<usize> {
         todo!()
     }
 
-    pub async fn delete_char(&self, p0: _, p1: i32) {
-        todo!()
-    }
-    pub async fn insert_char(&self, p0: _, p1: i32, p2: char) {
+    pub async fn set_cursor_position(&self, buffer_id: BufferId, position: i32) -> ServerResult<()> {
         todo!()
     }
 
-    pub async fn get_buffer_content(&self, p0: _) {
+    pub async fn delete_char(&self, buffer_id: BufferId, position: i32) -> ServerResult<()> {
+        todo!()
+    }
+    pub async fn insert_char(&self, buffer_id: BufferId, position: i32, ch: char) -> ServerResult<()> {
+        todo!()
+    }
+
+    pub async fn get_buffer_content(&self, buffer_id: BufferId) -> ServerResult<String> {
         todo!()
     }
     pub fn buffer_count(&self) -> usize {
         todo!()
     }
-    pub async fn create_buffer(&self, p0: _, p1: Option<_>) {
+    pub async fn create_buffer(&self, client_id: ClientId, content: Option<String>) -> ServerResult<BufferId> {
         todo!()
     }
 
-    pub fn is_client_connected(&self, p0: _) -> bool {
+    pub fn is_client_connected(&self, client_id: ClientId) -> bool {
         todo!()
     }
 
-    pub async fn disconnect_client(&self, client_id: ClientId) {
+    pub async fn disconnect_client(&self, client_id: ClientId) -> ServerResult<()>{
         todo!()
     }
 
