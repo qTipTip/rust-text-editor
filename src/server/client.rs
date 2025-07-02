58943ee26cbe411c2a030602fa6addef34f1e833
Ivar Stangeby <istangeby@gmail.com>
2025-07-02 13:30:30 +0200

    Add Client constructor

--- src/server/client.rs
+++ src/server/client.rs
@@ -1,5 +1,16 @@
-use crate::server::events::EditorEvent;
+use std::collections::HashSet;
+use crate::server::events::{BufferId, EditorEvent};
 
 pub struct Client {
-    editor_events: Vec<EditorEvent>
+    editor_events: Vec<EditorEvent>,
+    subscribed_buffers: HashSet<BufferId>
+}
+
+impl Client {
+    pub(crate) fn new() -> Self{
+        Self {
+            editor_events: Vec::new(),
+            subscribed_buffers: HashSet::new()
+        }
+    }
 }
